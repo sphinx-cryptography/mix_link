c0a7129171dfdbebf770368a1c3533096c5308a4
David Stainton <dstainton415@gmail.com>
2021-01-23 18:48:19 +0000

    Use Noise_XXhfs_25519+Kyber1024_ChaChaPoly_BLAKE2b

--- src/constants.rs
+++ src/constants.rs
@@ -14,15 +14,15 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pub const NOISE_PARAMS: & str = "Noise_XX_25519_ChaChaPoly_BLAKE2b";
-pub const PROLOGUE: [u8;1] = [0u8;1];
+pub const NOISE_PARAMS: & str = "Noise_XXhfs_25519+Kyber1024_ChaChaPoly_BLAKE2b";
+pub const PROLOGUE: [u8;1] = [1u8;1];
 pub const PROLOGUE_SIZE: usize = 1;
 pub const NOISE_MESSAGE_MAX_SIZE: usize = 65535;
 pub const KEY_SIZE: usize = 32;
 pub const MAC_SIZE: usize = 16;
 pub const MAX_ADDITIONAL_DATA_SIZE: usize = 255;
 pub const AUTH_MESSAGE_SIZE: usize = 1 + 8 + MAX_ADDITIONAL_DATA_SIZE;
-pub const NOISE_HANDSHAKE_MESSAGE1_SIZE: usize = PROLOGUE_SIZE + KEY_SIZE;
-pub const NOISE_HANDSHAKE_MESSAGE2_SIZE: usize = 360;
+pub const NOISE_HANDSHAKE_MESSAGE1_SIZE: usize = 1600 + PROLOGUE_SIZE;
+pub const NOISE_HANDSHAKE_MESSAGE2_SIZE: usize = 1680 + AUTH_MESSAGE_SIZE;
 pub const NOISE_HANDSHAKE_MESSAGE3_SIZE: usize = 328;
 pub const NOISE_MESSAGE_HEADER_SIZE: usize = MAC_SIZE + 4;
